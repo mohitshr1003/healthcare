97f34384f4423081ca4aaba4d15a599bc3af14bd
mohitshr1003 <mohitshr1003@gmail.com>
2022-08-04 16:44:19 +0530

    patient model created

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class PatientDetails(models.Model):
+    patient_name = models.CharField(max_length=30, null=True)
+    patient_id = models.CharField(max_length=6, null=True)
+    gender = models.CharField(max_length=10, null=True)
+    phone = models.IntegerField(null=True)
+    dob = models.DateField(max_length=200, null=True)
+    password = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return str(self.patient_id)
+
+class DoctorDetails(models.Model):
+    pass
