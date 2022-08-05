2e50fbe8daa57dc876564f8fee9ee647773e8f5b
mohitshr1003 <mohitshr1003@gmail.com>
2022-08-05 10:42:54 +0530

    reception model created

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,2 @@
 from django.contrib import admin
 
-from home.models import *
-
-# Register your models here.
-admin.site.register(PatientDetails)
-admin.site.register(DoctorDetails)
--- reception/models.py
+++ reception/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class PatientDetails(models.Model):
+    patient_name = models.CharField(max_length=30, null=True)
+    patient_id = models.CharField(max_length=6, null=True)
+    patient_gender = models.CharField(max_length=10, null=True)
+    patient_phone = models.IntegerField(null=True)
+    patient_dob = models.DateField(max_length=200, null=True)
+    patient_password = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return str(self.patient_id)
+
+class DoctorDetails(models.Model):
+    pass
