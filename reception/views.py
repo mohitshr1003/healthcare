679a2f1e3ea3b7f5421ee9a6b6b58bfabeb34cf4
mohitshr1003 <mohitshr1003@gmail.com>
2022-08-10 12:32:23 +0530

    initial

--- reception/views.py
+++ reception/views.py
@@ -9,10 +9,9 @@ from django.contrib.auth.hashers import make_password
 
 # Create your views here.
 def reception(request):
-    
     # for USER ID
     N = 3
-    alpha_id = random.choices(s.ascii_uppercase, k=N)
+    alpha_id = random.choices(s.ascii_uppercase, k=N)  # ABH254
     num_id = random.choices(s.digits, k=N)
     p_id = str(''.join(alpha_id + num_id))
 
@@ -35,20 +34,21 @@ def reception(request):
         p_mob = request.POST.get('pmob')
 
         patient_registration = PatientDetails(
-            patient_name = p_name,
-            patient_id = p_id,
-            patient_gender = p_gender,
-            patient_phone = p_mob,
-            patient_dob = p_dob,
-            patient_password = make_password(p_password)
+            patient_name=p_name,
+            patient_id=p_id,
+            patient_gender=p_gender,
+            patient_phone=p_mob,
+            patient_dob=p_dob,
+            patient_password=make_password(p_password)
         )
         user = User.objects.create_user(
-            p_id, 
-            None, 
+            p_id,
+            None,
             p_password
             )
-        user.save()   
+        user.save()
     return render(request, 'reception.html')
+
 
 def register_doctor(request):
     return render(request, 'registerdoctor.html')
